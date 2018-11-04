ba33d7efb9a593839a3ebc02919bf1e8d9eefc13
a lan <bluenowte97@gmail.com>
2018-11-04 19:58:42 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
 if speed < 40 || speed > 60
   return true
-else 
-  return false  
+else
+  return false
 end
 
 
@@ -10,6 +10,3 @@ end
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? true : false
 end
-	
-
-
